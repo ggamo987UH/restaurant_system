c234d41293efe0e3fb4f621c2ee26a10dcb9c660
ggamo987 <gemedran@cougarnet.uh.edu>
2022-10-26 15:40:38 -0500

    navbar

--- reservation-app/src/App.js
+++ reservation-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Bookings from './components/Pages/bookings';
+import Home from './components/Pages';
+import Calendar from './components/Pages/calendar';
+import Reporting from './components/Pages/reporting';
+import Seating from './components/Pages/seating';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/bookings' component={Bookings} />
+        <Route path='/calendar' component={Calendar} />
+        <Route path='/reporting' component={Reporting} />
+        <Route path='/seating' component={Seating} />
+        
+      </Routes>
+    </Router>
   );
 }
 
