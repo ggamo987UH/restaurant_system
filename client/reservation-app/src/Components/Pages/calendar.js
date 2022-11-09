0e60831c628df59d5e8bb911eb9bc103839c166f
nickinkaty <46138376+nickinkaty@users.noreply.github.com>
2022-11-09 14:56:43 -0600

    Calendar page

--- client/reservation-app/src/Components/Pages/calendar.js
+++ client/reservation-app/src/Components/Pages/calendar.js
@@ -1,8 +1,36 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+const events = [
+  {
+    id: 1,
+    title: "Reservation: Nick",
+    start: "2022-11-09T13:00:00",
+    end: "2022-11-09T18:00:00",
+  },
+  {
+    id: 2,
+    title: "Reservation: Iliyan",
+    start: "2022-11-11T13:00:00",
+    end: "2022-11-11T18:00:00",
+  },
+];
+
 function calendar() {
   return (
     <div>
       <h1>Calendar</h1>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          center: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        events={events}
+        initialView="dayGridMonth"
+      />
     </div>
   );
 }
