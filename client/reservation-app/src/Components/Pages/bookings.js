c59b6025bf5362cc0b3062c95ca40dd8c409feec
iliyansunasara <98232600+iliyansunasara@users.noreply.github.com>
2022-11-09 20:52:26 -0600

    Bookings Page
    Created bookings page and css

--- client/reservation-app/src/Components/Pages/bookings.js
+++ client/reservation-app/src/Components/Pages/bookings.js
@@ -1,8 +1,20 @@
 import React from "react";
+//get style sheet
+import "../../Styles/bookings.css";
+
 function bookings() {
-  return (
-    <div>
-      <h1>Bookings</h1>
+  return ( 
+    <div className="divPadding">
+      <form className="formStyle">
+        <h1 className="formTitle">Bookings</h1>
+        <label for="partySize">Party Size: </label>
+        <input type="number" className="formElement" name="partySize" max="16" required/><br></br>
+        <label for="partyTime">Party Time: </label>
+        <input type="time" className="formElement" name="partyTime" required/><br></br>
+        <label for="partyDate">Party Date: </label>
+        <input type="date" className="formElement" name="partyDate" required/><br></br><br></br>
+        <button type="submit" className="formElement">Submit</button>
+      </form>
     </div>
   );
 }
