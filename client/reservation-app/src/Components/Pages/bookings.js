83753c6b172ae1823b5b60b37b3fa96925e5f670
iliyansunasara <98232600+iliyansunasara@users.noreply.github.com>
2022-11-09 23:29:08 -0600

    some css tingz

--- client/reservation-app/src/Components/Pages/bookings.js
+++ client/reservation-app/src/Components/Pages/bookings.js
@@ -1,19 +1,32 @@
 import React from "react";
-//get style sheet
+// import Axios from "axios";
 import "../../Styles/bookings.css";
 
 function bookings() {
+
+  // const addBooking = () => {
+  //   Axios.post("http://localhost:3001/bookings", {
+  //     name: "test",
+  //     email: "test",
+  //     phone: "test",
+  //     date: "test",
+  //     time: "test",
+  //     people: "test",
+  //     message: "test",
+  //   });
+  // };
   return ( 
     <div className="divPadding">
       <form className="formStyle">
-        <h1 className="formTitle">Bookings</h1>
+        <h1 className="formTitle">BOOKINGS</h1>
         <label for="partySize">Party Size: </label>
         <input type="number" className="formElement" name="partySize" max="16" required/><br></br>
         <label for="partyTime">Party Time: </label>
         <input type="time" className="formElement" name="partyTime" required/><br></br>
         <label for="partyDate">Party Date: </label>
-        <input type="date" className="formElement" name="partyDate" required/><br></br><br></br>
-        <button type="submit" className="formElement">Submit</button>
+        <input type="date" className="formElement" name="partyDate" required/><br></br>
+        {/* <button type="submit" className="formElement" value="Submit" onClick={addBooking}>Submit</button> */}
+        <button type="submit" className="buttonStyle" value="Submit">Submit</button>
       </form>
     </div>
   );
