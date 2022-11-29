3fd270961595636d0b6b8d4b8341a89404c4b670
nickinkaty <46138376+nickinkaty@users.noreply.github.com>
2022-11-28 22:27:58 -0600

    combine booking submit button to one

--- client/reservation-app/src/Components/Pages/bookings.js
+++ client/reservation-app/src/Components/Pages/bookings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import "../../Styles/bookings.css";
 import { motion } from "framer-motion";
@@ -6,28 +6,44 @@ import { motion } from "framer-motion";
 import ModalOptions from "../login/modal/modal";
 //import Register from "../login/registration";
 function Bookings() {
+  const [nameD, setName] = useState("");
+  const [phoneD, setPhone] = useState("");
+  const [emailD, setEmail] = useState("");
+  const [partyD, setPartySize] = useState("");
+  const [timeD, setPartyTime] = useState("");
+  const [dateD, setPartyDate] = useState("");
+  const [creditD, setCreditCard] = useState("");
+  const [userType, setUserType] = useState("");
+  const [isBookingClicked, setIsBookingClicked] = useState(false);
 
-  const [nameD, setName] = useState('')
-  const [phoneD, setPhone] = useState('')
-  const [emailD, setEmail] = useState('')
-  const [partyD, setPartySize] = useState('')
-  const [timeD, setPartyTime] = useState('')
-  const [dateD, setPartyDate] = useState('')
-  const [creditD, setCreditCard] = useState('')
+  const onClickBooking = () => {
+    setIsBookingClicked(true);
+    if (userType === "Guest") {
+      insertBookings();
+    }
+  };
 
   const insertBookings = () => {
     var dayOfWeek = new Date(dateD).getDay();
     var datevar = dateD;
-    var dateNoYear = datevar.slice(5, 10)
+    var dateNoYear = datevar.slice(5, 10);
 
     if (creditD.length === 0 || creditD === null) {
       if (dayOfWeek === 5 || dayOfWeek === 6) {
-        alert("ERROR! Credit Card is required for weekend reservations.")
-        return
+        alert("ERROR! Credit Card is required for weekend reservations.");
+        return;
       }
-      if (dateNoYear === '01-01' || dateNoYear === '12-31' || dateNoYear === '12-25' || dateNoYear === '12-24' || dateNoYear === '07-04' || dateNoYear === '11-24' || dateNoYear === '05-29') {
-        alert('ERROR! Credit Card is required for holiday reservations.')
-        return
+      if (
+        dateNoYear === "01-01" ||
+        dateNoYear === "12-31" ||
+        dateNoYear === "12-25" ||
+        dateNoYear === "12-24" ||
+        dateNoYear === "07-04" ||
+        dateNoYear === "11-24" ||
+        dateNoYear === "05-29"
+      ) {
+        alert("ERROR! Credit Card is required for holiday reservations.");
+        return;
       }
     }
 
@@ -37,82 +53,161 @@ function Bookings() {
       console.log(response);
     });
 
-
-    Axios.post('http://localhost:3001/insertBookings', {
+    Axios.post("http://localhost:3001/insertBookings", {
       name: nameD,
       phone: phoneD,
       email: emailD,
       credit: creditD,
       partySize: partyD,
       partyTime: timeD,
-      partyDate: dateD
+      partyDate: dateD,
     }).then((result) => {
       alert(result.data.message);
     });
   };
 
-  return (
-  <div className="bookings">
-  <div className="bookingsBackground">
-  <motion.div exit={{ opacity: 0 }} initial={{ scale: .85, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ type: "tween", stiffness: 260, damping: 20 }}>
-    <div className="container">
-      <div class="title">Book Table</div>
-      <div class="content">Disclaimer: There will be a $10 no-show fee if you do not show up for the reservations placed on holidays or weekends</div>
-        <form action="#">
-          <div class="user__details">
-            <div class="input__box">
-              <span class="details">Full Name</span>
-              <input type="text" className="formElement" name="name" placeholder="..." onChange={(e) => setName(e.target.value)} required />
-            </div>
-            <div class="input__box">
-              <span class="details">Party Size </span>
-              <input type="number" className="formElement" name="partySize" max="8" min="1" defaultValue="0" onChange={(e) => setPartySize(e.target.value)} required />
-            </div>
-            <div class="input__box">
-              <span class="details">Email</span>
-              <input type="text" className="formElement" name="email" placeholder="..." onChange={(e) => setEmail(e.target.value)} required />
-            </div>
-            <div class="input__box">
-              <span class="details">Phone Number</span>
-              <input type="text" className="formElement" name="phone" placeholder="..." onChange={(e) => setPhone(e.target.value)} required />
-            </div>
-            <div class="input__box">
-              <span class="details">Party Date</span>
-              <input type="date" className="formElement" name="partyDate" onChange={(e) => setPartyDate(e.target.value)} min="2022-11-23" required />
-            </div>
-            <div class="input__box">
-              <span class="details">Party Time</span>
-              <input type="time" className="formElement" name="partyTime" step="3600" min="11:00" max="22:00" onChange={(e) => setPartyTime(e.target.value)} required />
-            </div>
-            <div class="input__box">
-              <span class="details">Credit Card Number:</span>
-              <input type="text" className="formElement" name="creditCard" placeholder="..." maxLength={16} onChange={(e) => setCreditCard(e.target.value)} />
-            </div>
-          </div>
-          <div class="gender__details">
-            <input type="radio" name="gender" id="dot-1" />
-            <input type="radio" name="gender" id="dot-2" />
-           
-            <span class="gender__title">Continue as: </span>
-            <div class="category">
-              <label for="dot-1">
-                <span class="dot one"></span>
-                <span>Guest</span>
-              </label>
-              <label for="dot-2">
-                <span class="dot two"></span>
-                <span>Member</span>
-              </label>
-            </div>
-          </div>
-          <button type="submit" className="buttonStyle" value="Submit" onClick={insertBookings}>Submit Booking</button>
+  useEffect(() => {
+    console.log(isBookingClicked);
+    console.log(userType);
+  }, [isBookingClicked]);
 
-          <ModalOptions />
-        </form>
+  return (
+    <div className="bookings">
+      <div className="bookingsBackground">
+        <motion.div
+          exit={{ opacity: 0 }}
+          initial={{ scale: 0.85, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ type: "tween", stiffness: 260, damping: 20 }}
+        >
+          <div className="container">
+            <div class="title">Book Table</div>
+            <div class="content">
+              Disclaimer: There will be a $10 no-show fee if you do not show up
+              for the reservations placed on holidays or weekends
+            </div>
+            <form action="#" onSubmit={(e) => e.preventDefault()}>
+              <div class="user__details">
+                <div class="input__box">
+                  <span class="details">Full Name</span>
+                  <input
+                    type="text"
+                    className="formElement"
+                    name="name"
+                    placeholder="..."
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Party Size </span>
+                  <input
+                    type="number"
+                    className="formElement"
+                    name="partySize"
+                    max="8"
+                    min="1"
+                    defaultValue="0"
+                    onChange={(e) => setPartySize(e.target.value)}
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Email</span>
+                  <input
+                    type="text"
+                    className="formElement"
+                    name="email"
+                    placeholder="..."
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Phone Number</span>
+                  <input
+                    type="text"
+                    className="formElement"
+                    name="phone"
+                    placeholder="..."
+                    onChange={(e) => setPhone(e.target.value)}
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Party Date</span>
+                  <input
+                    type="date"
+                    className="formElement"
+                    name="partyDate"
+                    onChange={(e) => setPartyDate(e.target.value)}
+                    min="2022-11-23"
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Party Time</span>
+                  <input
+                    type="time"
+                    className="formElement"
+                    name="partyTime"
+                    step="3600"
+                    min="11:00"
+                    max="22:00"
+                    onChange={(e) => setPartyTime(e.target.value)}
+                    required
+                  />
+                </div>
+                <div class="input__box">
+                  <span class="details">Credit Card Number:</span>
+                  <input
+                    type="text"
+                    className="formElement"
+                    name="creditCard"
+                    placeholder="..."
+                    maxLength={16}
+                    onChange={(e) => setCreditCard(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div
+                class="gender__details"
+                onChange={(e) => setUserType(e.target.value)}
+              >
+                <input type="radio" name="gender" id="dot-1" value="Guest" />
+                <input type="radio" name="gender" id="dot-2" value="Member" />
+
+                <span class="gender__title">Continue as: </span>
+                <div class="category">
+                  <label for="dot-1">
+                    <span class="dot one"></span>
+                    <span>Guest</span>
+                  </label>
+                  <label for="dot-2">
+                    <span class="dot two"></span>
+                    <span>Member</span>
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="buttonStyle"
+                value="Submit"
+                onClick={onClickBooking}
+              >
+                Submit Booking
+              </button>
+              {userType === "Member" && isBookingClicked === true && (
+                <ModalOptions
+                  show={true}
+                  setIsBookingClicked={setIsBookingClicked}
+                />
+              )}
+            </form>
+          </div>
+        </motion.div>
+      </div>
     </div>
-  </motion.div>
-  </div>
-  </div>
   );
 }
 export default Bookings;
--- client/reservation-app/src/Components/login/modal/modal.js
+++ client/reservation-app/src/Components/login/modal/modal.js
@@ -4,37 +4,39 @@ import Button from "react-bootstrap/Button";
 import "./modal.css";
 import Options from "./options";
 
-const ModalOptions = () => {
-    const [show, setShow] = React.useState(false);
-    
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
-    return (
-        <>
-        <Button variant="primary" onClick={handleShow}>
-            submit
-        </Button>
-    
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Are you a new user ?</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Options />
+const ModalOptions = (props) => {
+  const [show, setShow] = React.useState(props.show);
 
-            </Modal.Body>
-            <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-                Save Changes
-            </Button>
-            </Modal.Footer>
-        </Modal>
-        </>
-    );
-    }
+  const handleClose = () => {
+    props.setIsBookingClicked(false);
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      {/*<Button variant="primary" onClick={handleShow}>
+            submit
+        </Button>*/}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you a new user ?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Options />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default ModalOptions;
